ec0b6a23b8dba956bf62b9789183afcda258e071
nicole242-byte <nicolejanegallardo242@gmail.com>
2026-05-03 13:54:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2065,4 +2065,6 @@ if __name__ == "__main__":
 #║   URL:     http://127.0.0.1:5000                     ║
 #╚══════════════════════════════════════════════════════╝
 #    """)
-    app.run(debug=True, port=5000)
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
